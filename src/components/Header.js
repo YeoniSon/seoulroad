0622d8ac21f3cac60d877dc6b4319da23b708342
YeoniSon <sjy8211@gmail.com>
2024-03-10 20:51:24 +0900

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logoImg from "../images/logo.png";
 import UserMenu from "./UserMenu";
 import styles from "./Header.module.css";
 
 function Header() {
+  const getLinkStyle = ({ isActive }) => ({
+    textDecoration: isActive ? "underline" : "",
+  });
+
   return (
     <div className={styles.nav}>
       <div className={styles.container}>
@@ -13,16 +17,24 @@ function Header() {
         </Link>
         <ul className={styles.menu}>
           <li>
-            <Link to={"/food"}>먹거리</Link>
+            <NavLink style={getLinkStyle} to={"/food"}>
+              먹거리
+            </NavLink>
           </li>
           <li>
-            <Link to={"/culture"}>문화</Link>
+            <NavLink style={getLinkStyle} to={"/culture"}>
+              문화
+            </NavLink>
           </li>
           <li>
-            <Link to={"/healing"}>힐링</Link>
+            <NavLink style={getLinkStyle} to={"/healing"}>
+              힐링
+            </NavLink>
           </li>
           <li>
-            <Link to={"/mypage"}>마이페이지</Link>
+            <NavLink style={getLinkStyle} to={"/mypage"}>
+              마이페이지
+            </NavLink>
           </li>
           <li>
             <UserMenu />
